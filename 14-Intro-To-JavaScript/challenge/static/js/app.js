63c9f0de070244a7c6ef6bdf2848c2bb4e5540e7
stephanie rivas <steph.rivas25@gmail.com>
2021-04-09 11:00:11 -0400

    homework

--- 14-Intro-To-JavaScript/challenge/static/js/app.js
+++ 14-Intro-To-JavaScript/challenge/static/js/app.js
@@ -21,3 +21,25 @@ var button = d3.select("#filter-btn");
 
 //create event handlers for clicking the button
 button.on("click", runEnter);
+
+function runEnter() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  var filteredData = tableData.filter((date) => date.datetime === inputValue);
+
+  console.log(filteredData);
+  //filter for date
+  // tableData.filter((datum) => {
+  //     datum.datetime ===
+  //   })
+  tbody.html("")
+  filteredData.forEach((ufoDatetime) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoDatetime).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
